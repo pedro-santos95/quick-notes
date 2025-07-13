2151aee63cfbc29cfe6981d7b72e5331b221abc4
PedroSantoss-Dev <ph.senasantos@gmail.com>
2025-07-13 17:13:30 -0300

    inital commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+   <div>
+      <h1 className="text-2xl font-bold mb-4">Minhas Anotações</h1>
+      <p>Bem-vindo! Faça login para acessar suas notas.</p>
+  </div>
+  );
+}
